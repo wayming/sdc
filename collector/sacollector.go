b0912d96ec10318e27bbb34847590eba8b911498
way <wayming.z@gmail.com>
2024-04-07 15:03:31 +0000

    time series table

--- collector/sacollector.go
+++ collector/sacollector.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"errors"
 	"log"
+	"regexp"
 	"strings"
 
 	"github.com/wayming/sdc/dbloader"
@@ -28,34 +29,31 @@ func NewSACollector(loader dbloader.DBLoader, logger *log.Logger, schema string)
 	return &collector
 }
 
-func (collector *SACollector) TraverseHTML(node *html.Node) (map[string]string, error) {
+func (collector *SACollector) DecodeDualTableHTML(node *html.Node) (map[string]string, error) {
 	var indicatorsMap map[string]string
 	var err error
 	if node.Type == html.ElementNode {
 		for _, attr := range node.Attr {
 			if attr.Key == "data-test" && attr.Val == "overview-info" {
-				indicatorsMap, err = collector.ConvetHTMLTableToMap(node)
+				indicatorsMap, err = collector.DecodeSimpleTable(node)
 				if err != nil {
 					return nil, errors.New("Faield to decode html table overview-info. Error: " + err.Error())
 				}
 
 			}
 			if attr.Key == "data-test" && attr.Val == "overview-quote" {
-				indicatorsMap, err = collector.ConvetHTMLTableToMap(node)
+				indicatorsMap, err = collector.DecodeSimpleTable(node)
 				if err != nil {
 					return nil, errors.New("Faield to decode html table overview-quote. Error: " + err.Error())
 				}
 
 			}
-			// if attr.Key == "data-test" && attr.Val == "financials" {
-			// 	collector.TraverseTable(node)
-			// }
 		}
 	}
 
 	for child := node.FirstChild; child != nil; child = child.NextSibling {
 		var moreIndicators map[string]string
-		if moreIndicators, err = collector.TraverseHTML(child); err != nil {
+		if moreIndicators, err = collector.DecodeDualTableHTML(child); err != nil {
 			return nil, err
 		}
 		if indicatorsMap, err = concatMaps(indicatorsMap, moreIndicators); err != nil {
@@ -64,6 +62,32 @@ func (collector *SACollector) TraverseHTML(node *html.Node) (map[string]string, 
 	}
 
 	return indicatorsMap, nil
+}
+
+func (collector *SACollector) DecodeTimeSeriesTableHTML(node *html.Node) ([]map[string]string, error) {
+	if node.Type == html.ElementNode {
+		for _, attr := range node.Attr {
+			if attr.Key == "data-test" && attr.Val == "financials" {
+				indicatorMaps, err := collector.DecodeTimeSeriesTable(node)
+				if err != nil {
+					return nil, errors.New("Faield to decode html table overview-quote. Error: " + err.Error())
+				}
+				return indicatorMaps, nil
+			}
+		}
+	}
+
+	for child := node.FirstChild; child != nil; child = child.NextSibling {
+		indicatorMaps, err := collector.DecodeTimeSeriesTableHTML(child)
+		if err != nil {
+			return nil, err
+		}
+		if len(indicatorMaps) > 0 {
+			return indicatorMaps, nil
+		}
+	}
+
+	return nil, nil
 }
 
 func (collector *SACollector) FirstTextNode(node *html.Node) *html.Node {
@@ -80,7 +104,7 @@ func (collector *SACollector) FirstTextNode(node *html.Node) *html.Node {
 	return nil
 }
 
-func (collector *SACollector) ConvetHTMLTableToMap(node *html.Node) (map[string]string, error) {
+func (collector *SACollector) DecodeSimpleTable(node *html.Node) (map[string]string, error) {
 	stockOverview := make(map[string]string)
 	// tbody
 	tbody := node.FirstChild
@@ -92,6 +116,9 @@ func (collector *SACollector) ConvetHTMLTableToMap(node *html.Node) (map[string]
 			text1 := collector.FirstTextNode(td)
 			if text1 != nil {
 				collector.logger.Println(text1.Data)
+			} else {
+				// No text node for this sibling, try next one
+				continue
 			}
 
 			for td2 := td.NextSibling; td2 != nil; td2 = td2.NextSibling {
@@ -108,8 +135,77 @@ func (collector *SACollector) ConvetHTMLTableToMap(node *html.Node) (map[string]
 
 }
 
+func (collector *SACollector) DecodeTimeSeriesTable(node *html.Node) ([]map[string]string, error) {
+	completeSeries := make([]map[string]string, 0)
+	// thead
+	thead := node.FirstChild
+
+	pattern := `[a-zA-Z]+`
+	re := regexp.MustCompile(pattern)
+
+	// For each tr
+	for tr := thead.FirstChild; tr != nil; tr = tr.NextSibling {
+		td := tr.FirstChild
+		if td != nil {
+			text1 := collector.FirstTextNode(td)
+			if text1 != nil {
+				collector.logger.Println(text1.Data)
+			}
+
+			for td2 := td.NextSibling; td2 != nil; td2 = td2.NextSibling {
+				text2 := collector.FirstTextNode(td2)
+				if text2 != nil {
+					dataPoint := make(map[string]string)
+					collector.logger.Println(text2.Data)
+					if matches := re.FindAllString(text2.Data, -1); len(matches) > 0 {
+						collector.logger.Println("ignore ", text2.Data)
+						continue
+					}
+					dataPoint[text1.Data] = text2.Data
+					completeSeries = append(completeSeries, dataPoint)
+					continue
+				}
+			}
+		}
+	}
+
+	// tbody
+	if thead.NextSibling == nil || thead.NextSibling.NextSibling == nil {
+		return nil, errors.New("unexpected structure. Can not find the tbody element")
+	}
+	tbody := thead.NextSibling.NextSibling
+	// For each tr
+	for tr := tbody.FirstChild; tr != nil; tr = tr.NextSibling {
+		td := tr.FirstChild
+		if td != nil {
+			text1 := collector.FirstTextNode(td)
+			if text1 != nil {
+				collector.logger.Println(text1.Data)
+			} else {
+				continue
+			}
+
+			idx := 0
+			// Assumes the same amount of tds as the the thead
+			for td2 := td.NextSibling; td2 != nil && idx < len(completeSeries); td2 = td2.NextSibling {
+				if td2.Type == html.ElementNode && td2.Data == "td" {
+					text2 := collector.FirstTextNode(td2)
+					if text2 != nil {
+						collector.logger.Println(text2.Data)
+
+						completeSeries[idx][text1.Data] = text2.Data
+					}
+					idx++
+				}
+			}
+		}
+	}
+	return completeSeries, nil
+
+}
+
 // Parse Stock Analysis page and return JSON text
-func (collector *SACollector) ReadStockAnalysisPage(url string, params map[string]string) (string, error) {
+func (collector *SACollector) ReadStockAnalysisOverallPage(url string, params map[string]string) (string, error) {
 	htmlContent, err := ReadURL(url, params)
 	if err != nil {
 		return "", err
@@ -120,7 +216,34 @@ func (collector *SACollector) ReadStockAnalysisPage(url string, params map[strin
 		return "", errors.New("Failed to parse the html page " + url + ". Error: " + err.Error())
 	}
 
-	indicatorsMap, err := collector.TraverseHTML(htmlDoc)
+	indicatorsMap, err := collector.DecodeDualTableHTML(htmlDoc)
+	if err != nil {
+		return "", errors.New("Failed to parse " + url + ". Error: " + err.Error())
+	}
+
+	jsonData, err := json.Marshal(indicatorsMap)
+	if err != nil {
+		return "", errors.New("Failed to marshal stock data to JSON text. Error: " + err.Error())
+	} else {
+		collector.logger.Println("JSON text generated - " + string(jsonData))
+
+	}
+	return string(jsonData), nil
+}
+
+// Parse Stock Analysis page and return JSON text
+func (collector *SACollector) ReadStockAnalysisTimeSeriesPage(url string, params map[string]string) (string, error) {
+	htmlContent, err := ReadURL(url, params)
+	if err != nil {
+		return "", err
+	}
+
+	htmlDoc, err := html.Parse(strings.NewReader(htmlContent))
+	if err != nil {
+		return "", errors.New("Failed to parse the html page " + url + ". Error: " + err.Error())
+	}
+
+	indicatorsMap, err := collector.DecodeTimeSeriesTableHTML(htmlDoc)
 	if err != nil {
 		return "", errors.New("Failed to parse " + url + ". Error: " + err.Error())
 	}
--- collector/sacollector_test.go
+++ collector/sacollector_test.go
@@ -72,21 +72,12 @@ func TestMSCollector_ReadStockAnalysisPage(t *testing.T) {
 	}
 	stockOverview := commonTestConfig
 	stockOverview.args.url = "https://stockanalysis.com/stocks/msft/"
-	financialsIncome := commonTestConfig
-	financialsIncome.args.url = "https://stockanalysis.com/stocks/msft/financials/?p=quarterly"
-	financialsBalanceShet := commonTestConfig
-	financialsBalanceShet.args.url = "https://stockanalysis.com/stocks/msft/financials/balance-sheet/?p=quarterly"
-	financialsCashFlow := commonTestConfig
-	financialsCashFlow.args.url = "https://stockanalysis.com/stocks/msft/financials/cash-flow-statement/?p=quarterly"
-	financialsRatios := commonTestConfig
-	financialsRatios.args.url = "https://stockanalysis.com/stocks/msft/financials/ratios/?p=quarterly"
-
 	tests := []struct {
 		name    string
 		fields  fields
 		args    args
 		wantErr bool
-	}{stockOverview, financialsIncome, financialsBalanceShet, financialsCashFlow, financialsRatios}
+	}{stockOverview}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -95,13 +86,80 @@ func TestMSCollector_ReadStockAnalysisPage(t *testing.T) {
 				tt.fields.logger,
 				tt.fields.dbSchema,
 			)
-			got, err := collector.ReadStockAnalysisPage(tt.args.url, tt.args.params)
+			got, err := collector.ReadStockAnalysisOverallPage(tt.args.url, tt.args.params)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("MSCollector.ReadStockAnalysisPage() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if len(got) == 0 {
 				t.Errorf("MSCollector.ReadStockAnalysisPage() fails to parse %s", tt.args.url)
+			}
+		})
+	}
+	teardownSATest()
+}
+
+func TestMSCollector_ReadStockAnalysisPageTimeSeries(t *testing.T) {
+	type fields struct {
+		dbLoader dbloader.DBLoader
+		logger   *log.Logger
+		dbSchema string
+	}
+	type args struct {
+		url    string
+		params map[string]string
+	}
+
+	setupSATest(t.Name())
+
+	commonTestConfig := struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		name: "LoadStockAnalysisPage",
+		fields: fields{
+			dbLoader: saTestDBLoader,
+			logger:   saTestLogger,
+			dbSchema: MS_TEST_SCHEMA_NAME,
+		},
+		args: args{
+			url:    "",
+			params: make(map[string]string, 0),
+		},
+		wantErr: false,
+	}
+	financialsIncome := commonTestConfig
+	financialsIncome.args.url = "https://stockanalysis.com/stocks/msft/financials/?p=quarterly"
+	// financialsBalanceShet := commonTestConfig
+	// financialsBalanceShet.args.url = "https://stockanalysis.com/stocks/msft/financials/balance-sheet/?p=quarterly"
+	// financialsCashFlow := commonTestConfig
+	// financialsCashFlow.args.url = "https://stockanalysis.com/stocks/msft/financials/cash-flow-statement/?p=quarterly"
+	// financialsRatios := commonTestConfig
+	// financialsRatios.args.url = "https://stockanalysis.com/stocks/msft/financials/ratios/?p=quarterly"
+
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{financialsIncome} //, financialsBalanceShet, financialsCashFlow, financialsRatios}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			collector := collector.NewSACollector(
+				tt.fields.dbLoader,
+				tt.fields.logger,
+				tt.fields.dbSchema,
+			)
+			got, err := collector.ReadStockAnalysisTimeSeriesPage(tt.args.url, tt.args.params)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("MSCollector.ReadStockAnalysisTimeSeriesPage() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if len(got) == 0 {
+				t.Errorf("MSCollector.ReadStockAnalysisTimeSeriesPage() fails to parse %s", tt.args.url)
 			}
 		})
 	}
